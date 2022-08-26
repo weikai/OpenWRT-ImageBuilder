72e8c44973667bdc4a05259de8e657860976e7ae
Weikai Zhang <weikai@gmail.com>
2022-08-25 21:29:51 -0400

    fix package issue

--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 Custom OpenWRT firmware for NanoPi-R4s and x86_64 with the following packages.
 
-luci luci-theme-bootstrap uhttpd
-nano curl rsync
-git-http
-fdisk cfdisk resize2fs
-kmod-fs-btrfs btrfs-progs
-kmod-wireguard luci-app-wireguard luci-proto-wireguard wireguard-tools
+-dnsmasq dnsmasq-full adguardhome luci luci-theme-bootstrap uhttpd
+nano curl rsync 
+git-http diffutils
+fdisk cfdisk resize2fs blkid
+kmod-usb-storage kmod-usb-storage-uas kmod-fs-ext4 kmod-fs-btrfs btrfs-progs
+kmod-wireguard luci-app-wireguard luci-proto-wireguard wireguard-tools qrencode
 block-mount blkid openssl-util ntpclient
 openvpn-openssl luci-app-openvpn
-acme luci-app-acme apache apache-mod-http2 apache-mod-proxy apache-mod-proxy-html apache-mod-ssl apache-mod-session-crypto apache-utils 
-adguardhome
+acme luci-app-acme
 ddns-scripts luci-app-ddns
 vnstat2 luci-app-vnstat2 collectd
 nlbwmon luci-app-nlbwmon
-restic restic-rest-server
 luci-app-sqm sqm-scripts sqm-scripts-extra
 etherwake postfix sysstat
+restic restic-rest-server
+apache apache-mod-http2 apache-mod-proxy apache-mod-proxy-html apache-mod-ssl apache-mod-session-crypto apache-utils 
 
 
 **!!! not tested extensively !!! use at own risk**
--- build.sh
+++ build.sh
@@ -1,65 +1,41 @@
 #!/bin/bash
 
+build=$1
+
 workdir=$(dirname $(readlink -e $0))
 
 . $workdir/.env
 
-for url in $urls; do
+for line in "${images[@]}"; do
+    imginfo=($(echo "$line"|tr ',' ' '))
+
+    NAME=${imginfo[0]}    
+    [[ -n $build ]] && [[ $NAME != $build ]] && continue    
+    PROFILE=${imginfo[1]}
+    url=${imginfo[2]}
+
+    echo "Building $NAME using profile $PROFILE from $url"
     img=$(basename $url)
     imagebuilder="$workdir/.builder/$img"
     
     [[ ! -d "$workdir/.builder" ]] && mkdir "$workdir/.builder"
     
     [[ ! -f $imagebuilder ]] && wget $url -O $imagebuilder
-    if [[ $url =~ \/targets\/.+\/openwrt-imagebuilder ]]; then
-        target=$(echo $url|sed -r 's|.*/targets/(.+)/openwrt-imagebuilder.*|\1|'|tr '/' '_')
+    
+    buildir="$workdir/build/$NAME"
+    [[ ! -d $buildir ]] && mkdir -p "$buildir" && [[ -z $(ls -1 "$buildir") ]] && \
+    tar xvf $imagebuilder --strip-components=1 -C $buildir #extract files
 
-        if [[ $target =~ rockchip ]]; then
-            PROFILE=friendlyarm_nanopi-r4s #run make info for a full list
-        else
-            PROFILE=generic #run make info for a full list
-        fi
-        
-        buildir="$workdir/build/$target"
-        [[ ! -d $buildir ]] && mkdir -p "$buildir" && [[ -z $(ls -1 "$buildir") ]] && \
-        tar xvf $imagebuilder --strip-components=1 -C $buildir #extract files
+    #update config
+    sed -r "s/(CONFIG_TARGET_KERNEL_PARTSIZE)=.*/\1=$KERNEL_PARTSIZE/" -i $buildir/.config
+    sed -r "s/(CONFIG_TARGET_ROOTFS_PARTSIZE)=.*/\1=$ROOTFS_PARTSIZE/" -i $buildir/.config
+    sed -r "s/^(CONFIG_GRUB_TIMEOUT)=(.*)/\1=$GRUB_TIMEOUT/" -i $buildir/.config
 
-        #update config
-        sed -r "s/(CONFIG_TARGET_KERNEL_PARTSIZE)=.*/\1=$KERNEL_PARTSIZE/" -i $buildir/.config
-        sed -r "s/(CONFIG_TARGET_ROOTFS_PARTSIZE)=.*/\1=$ROOTFS_PARTSIZE/" -i $buildir/.config
-        sed -r "s/^(CONFIG_GRUB_TIMEOUT)=(.*)/\1=$GRUB_TIMEOUT/" -i $buildir/.config
+    #build image
+    cd "$buildir" && \
+    make image PROFILE=$PROFILE PACKAGES="$(echo $PACKAGES|xargs)"
 
-        #build image
-        cd "$buildir" && \
-        make image PROFILE=$PROFILE PACKAGES="$(echo $PACKAGES|xargs)"
-
-    else
-        echo "Can't determind target by url"
-        exit 1
+    if [[ -n $build && -z $PROFILE ]]; then
+        echo "Build name $build not found"
     fi
 done
-
-exit
-buildir=$workdir/build
-imagebuilder=$workdir/openwrt-imagebuilder-22.03.0-rc6-x86-64.Linux-x86_64.tar.xz
-
-ROOTFS_PARTSIZE=1024 # default 104 megabyte
-KERNEL_PARTSIZE=16 # default 16 megabyte
-GRUB_TIMEOUT=1
-
-[ ! -d "$buildir" ] && mkdir "$buildir"
-
-#tar xvf $imagebuilder --strip-components=1 -C $buildir #extract files
-
-sed -r "s/(CONFIG_TARGET_KERNEL_PARTSIZE)=.*/\1=$KERNEL_PARTSIZE/" -i $buildir/.config
-sed -r "s/(CONFIG_TARGET_ROOTFS_PARTSIZE)=.*/\1=$ROOTFS_PARTSIZE/" -i $buildir/.config
-sed -r "s/^(CONFIG_GRUB_TIMEOUT)=(.*)/\1=$GRUB_TIMEOUT/" -i $buildir/.config
-
-
-
-#make image PROFILE="profile-name" PACKAGES="pkg1 pkg2 pkg3 -pkg4 -pkg5 -pkg6" FILES="files"
-
-cd "$buildir" && \
-make image PROFILE=$PROFILE PACKAGES="$(echo $PACKAGES|xargs)"
-
-
